923580b1fc3383f6f6b256d02b4d9256c046c580
Ganeshan <ganeshanmadesh36@outlook.com>
2023-02-22 15:09:57 +0530

    React app

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Header = () => {
+    return(
+        <div className='ui fixed menu'>            <div className='ui container center'>                <h2>NCG Batch 2023</h2>            </div>        </div>    )
+}
+export default Header;
